6b0c66eec17115bf7b49ee302c0e5274c112b213
Veigl042009 <andreas.veigl1@gmail.com>
2025-09-12 09:20:47 +0200

    h

---
+++ SEW3/01_HerbariumConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿
+// FJHerbariumLibrary.Herbarium = new FJHerbariumLibrary.Herbarium(); // full qualified
+using FJHerbariumLibrary;
+
+Herbarium myHerbarium = new Herbarium();
+myHerbarium.Generate("HerbariumTemplate2.pptx", @"C:\Users\andreas.veigl\Desktop", "Namen.txt", @"C:\Users\andreas.veigl\source\repos\Herbarium_Veigl");  // @ bedeuted, keine Escape Sequenzen für \
