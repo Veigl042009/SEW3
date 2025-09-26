be868b858c7fda05660ed71bfc7b07e72427b97b
Veigl042009 <andreas.veigl1@gmail.com>
2025-09-26 09:20:37 +0200

    neu

---
+++ SEW3/02_Files/Program.cs
@@ -0,0 +1,21 @@
+﻿string text = "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.\r\n\r\nLorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.";
+
+File.WriteAllText("output.txt", text);
+
+string[] names = ["Kamleitner", "Obernberger", "Kashofer", "Auer"];
+File.WriteAllLines("outputarry.txt", names);
+
+File.AppendAllText("output_append.txt", "hello"); // Beim erstenmal wird es erstellt und dann immer im dokument angehängt
+
+string[] plants = File.ReadAllLines("Namen.txt");
+
+//foreach(string plant in plants)
+///{
+//    Console.WriteLine(plant);
+//}
+
+Console.WriteLine(plants[5]);
+
+string[] plantDetails = plants[5].Split(',');
+
+Console.WriteLine(plantDetails[4]);
