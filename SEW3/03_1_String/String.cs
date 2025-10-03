3d86b894664694d59c17853976340a9851987fb1
Veigl042009 <andreas.veigl1@gmail.com>
2025-10-03 09:18:57 +0200

    neu

---
+++ SEW3/03_1_String/String.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_1_String
+{
+    class CaesarVerschluesselung
+    {
+        public string Verschluesseln(string eingabe, int schluessel)
+        {
+            string ausgabe = "";
+            for (int i = 0; i < eingabe.Length; i++)
+            {
+                char c = eingabe[i];
+                ausgabe += (char)(c + schluessel);
+            }
+            return ausgabe;
+        }
+    }
+}
