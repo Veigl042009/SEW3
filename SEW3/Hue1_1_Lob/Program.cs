27c967b9d95205bf02c307271316d750c308895c
Andreas <andreas.veigl1@gmail.com>
2025-10-06 19:56:59 +0200

    hü

---
+++ SEW3/Hue1_1_Lob/Program.cs
@@ -0,0 +1,31 @@
+﻿List<string> adjektive = new(File.ReadAllLines("adjektive.txt"));
+List<string> nomenM = new(File.ReadAllLines("nomen_m.txt"));
+List<string> nomenW = new(File.ReadAllLines("nomen_w.txt"));
+
+Console.Write("Wie viele Lobeszeilen möchtest du? ");
+int anzahl = int.Parse(Console.ReadLine());
+
+Console.Write("Geschlecht (m = männlich, w = weiblich): ");
+char geschlecht = Console.ReadLine()[0];
+
+Random rnd = new();
+
+for (int i = 0; i < anzahl; i++)
+{
+    string adj = adjektive[rnd.Next(adjektive.Count)];
+    string nomen;
+
+    if (geschlecht == 'm')
+        nomen = nomenM[rnd.Next(nomenM.Count)];
+    else
+        nomen = nomenW[rnd.Next(nomenW.Count)];
+
+    string artikel;
+
+    if (nomenW.Contains(nomen))
+        artikel = "die";
+    else
+        artikel = "der";
+
+    Console.WriteLine($"Du bist {artikel} {adj} {nomen}");
+}
---
+++ SEW3/Hue1_2_TeilnehmerListe/Program.cs
@@ -0,0 +1,18 @@
+﻿while (true)
+{
+    Console.Write("Vorname (oder e zum Beenden): ");
+    string vorname = Console.ReadLine();
+
+    if (vorname.ToLower() == "e")
+        break;
+
+    Console.Write("Nachname: ");
+    string nachname = Console.ReadLine();
+
+    Console.Write("Geburtsdatum: ");
+    string geburtsdatum = Console.ReadLine();
+
+    string daten = $"{vorname};{nachname};{geburtsdatum}\n";
+
+    File.AppendAllText("teilnehmer.csv", daten);
+}
