b905eae217061ae08bfe2e9f4f1046b9e1d7d6a2
Veigl042009 <andreas.veigl1@gmail.com>
2025-11-14 07:59:57 +0100

    neu

---
+++ SEW3/AANotizManagerLibrary/NotizManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AANotizManagerLibrary
+{
+    public class NotizManager
+
+    {
+        // Property für den Text der Notiz
+        public string NotizText { get; set; }
+
+        // Konstruktor: startet mit leerem Text
+        public NotizManager()
+        {
+            NotizText = "";
+        }
+
+        // Zählt die Wörter in der Notiz
+        public int ZaehleWoerter()
+        {
+            if (string.IsNullOrWhiteSpace(NotizText))
+                return 0;
+
+            string[] woerter = NotizText.Split(' ', '\n', '\t');
+            return woerter.Length;
+        }
+
+        // Speichert den Text in einer Datei und gibt true zurück, wenn erfolgreich
+        public bool SpeichereInDatei(string dateipfad)
+        {
+            try
+            {
+                File.WriteAllText(dateipfad, NotizText);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        // Lädt den Text aus einer Datei und gibt ihn zurück
+        public string LadeAusDatei(string dateipfad)
+        {
+            if (File.Exists(dateipfad))
+            {
+                NotizText = File.ReadAllText(dateipfad);
+            }
+            else
+            {
+                NotizText = "";
+            }
+
+            return NotizText;
+        }
+
+        // Ersetzt Text und gibt den neuen Text zurück
+        public string ErsetzeText(string alt, string neu)
+        {
+            NotizText = NotizText.Replace(alt, neu);
+            return NotizText;
+        }
+
+    }
+}
+
+
---
+++ SEW3/AAeins/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AAeins
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
