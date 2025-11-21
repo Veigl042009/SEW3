ed26a6d9f4b0b4ddb8f8cf3acc4ca44981f4b35d
Veigl042009 <andreas.veigl1@gmail.com>
2025-11-21 09:15:47 +0100

    besserung der BEispiele

--- SEW3/08_CallByReference/Program.cs
+++ SEW3/08_CallByReference/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _08_CallByReference;
+
+int a = 5;
+int result = Calculator.CalculateDouble(a);
+Console.WriteLine($"Ergebnis: {result}");
+Console.WriteLine(a);
+
+int result2;
+Calculator.CalculateDouble(a, out result2);      // Argument out kann mit einer uninitialisierten Variable aufgerufen werden
+Console.WriteLine($"Ergebnis: {result2}");
+
+Calculator.CalculateDouble(ref a);
+Console.WriteLine($"Ergebnis: {a}");
+
+// Konvertierung von Datentypen
+string? s = Console.ReadLine();
+int z;
+if(int.TryParse(s, out z))
+{
+ Console.WriteLine($"Konvertierung erfolgreich: {z}");
+}
+else
+{
+    Console.WriteLine("Konvertierung fehlgeschlagen");
+}
