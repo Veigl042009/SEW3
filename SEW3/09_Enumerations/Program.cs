d0f73055cad4b4452b473f7e141d44f419318cda
Veigl042009 <andreas.veigl1@gmail.com>
2025-12-05 07:48:48 +0100

    neu bsp

---
+++ SEW3/09_Enumerations/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Status homework = Status.Abgeschlossen;
+Console.WriteLine(homework);
+if(homework == Status.Abgeschlossen)
+{
+    Console.WriteLine("Super, du bist fleissig!");
+}
+
+public enum Status
+{
+    Erstellt,
+    Geplant,
+    Begonnen,
+    Pausiert,
+    Abgeschlossen,
+}
+
+
---
+++ SEW3/10_Turtle/Program.cs
@@ -0,0 +1,29 @@
+﻿using TurtleGraphics;
+using SixLabors.ImageSharp;
+
+Turtle.PenDown();
+Turtle.SetPenWidth(10);
+void printCross(int size, int level)
+{
+    
+    for (int i = 0; i < 4; i++)
+    {
+        if (level == 0)
+            return;   // Abbruchbedingung
+        Turtle.Forward(size);
+        Turtle.TurnLeft(45);
+        // hier ein weiteres Kreis
+        printCross(size * 2 / 3, level - 1);    // GRöße auf 2/5 reduzieren
+        Turtle.TurnRight(45);
+        Turtle.Back(size);
+        Turtle.TurnLeft(90);
+    }
+    
+}   
+
+printCross(50, 10);
+
+Turtle.ShowTurtle();
+
+
+
